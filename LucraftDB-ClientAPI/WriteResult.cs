39015db9d7b4d2c0591b9ba86e08e9e82d3ddb02
Luca Lewin <luca.lewin23@gmail.com>
2021-01-16 17:10:25 +0100

    create WriteResult

---
+++ LucraftDB-ClientAPI/WriteResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lucraft.Database.Client
+{
+    public class WriteResult
+    {
+        public long ElapsedTime { get; init; }
+        public long WriteTime { get; init; }
+    }
+}
