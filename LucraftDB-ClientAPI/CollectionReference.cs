0da69266e6172f4ced715db542a004ddf89e8a28
Luca Lewin <luca.lewin23@gmail.com>
2021-01-16 17:08:08 +0100

    add new methods + use LambdaToString from NuGet package  + also beautify method-code

--- LucraftDB-ClientAPI/CollectionReference.cs
+++ LucraftDB-ClientAPI/CollectionReference.cs
@@ -1,4 +1,5 @@
 ﻿using Lucraft.Database.Client.Query;
+using Lucraft.Utilities.Reflection;
 using Newtonsoft.Json;
 using System;
 using System.Linq.Expressions;
@@ -10,34 +11,41 @@ namespace Lucraft.Database.Client
         private string DbID { get; init; }
         public string ID { get; init; }
 
-        public CollectionReference(string dbID, string id)
+        internal CollectionReference(string dbID, string id)
         {
             DbID = dbID;
             ID = id;
         }
 
+        public DocumentReference GetDocument(string id)
+        {
+            return new DocumentReference(DbID, ID, id);
+        }
+
         public CollectionSnapshot Get()
         {
             string req = $"get /{DbID}/{ID}/*";
-            string res = DataStorage.Instance.MakeRequest(req);
+            string res = DataStorage.MakeRequest(req);
             return JsonConvert.DeserializeObject<CollectionSnapshot>(res);
         }
 
-        public void Set(object data)
+        public WriteResult Set(object data)
         {
-            DataStorage.Instance.MakeRequest($"get /{DbID}/{ID}/* {JsonConvert.SerializeObject(data)}");
+            string req = $"get /{DbID}/{ID}/* {JsonConvert.SerializeObject(data)}";
+            string res = DataStorage.MakeRequest(req);
+            return JsonConvert.DeserializeObject<WriteResult>(res);
         }
 
         public QuerySnapshot Query(string queryStr)
         {
             string req = $"get /{DbID}/{ID}/* where {queryStr}";
-            string res = DataStorage.Instance.MakeRequest(req);
+            string res = DataStorage.MakeRequest(req);
             return JsonConvert.DeserializeObject<QuerySnapshot>(res);
         }
 
         public QuerySnapshot Query<T>(Expression<Func<T, bool>> queryExpr)
         {
-            string exprStr = LambdaUtilities.LambdaToString<T>(queryExpr);
+            string exprStr = Lambda.LambdaToString(queryExpr);
             string paramName = queryExpr.Parameters[0].Name;
             foreach (var property in typeof(T).GetProperties())
             {
