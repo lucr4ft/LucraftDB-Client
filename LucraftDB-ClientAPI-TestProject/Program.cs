bad828e547e9807fe88b9f23769aeaa1e30f7290
Luca Lewin <luca.lewin23@gmail.com>
2021-01-16 17:11:25 +0100

    add new tests

--- LucraftDB-ClientAPI-TestProject/Program.cs
+++ LucraftDB-ClientAPI-TestProject/Program.cs
@@ -1,7 +1,5 @@
 ﻿using Lucraft.Database.Client.Query;
 using System;
-using System.Linq.Expressions;
-using System.Threading.Tasks;
 
 namespace Lucraft.Database.Client.Test
 {
@@ -12,12 +10,29 @@ namespace Lucraft.Database.Client.Test
 
             //(email == value(Lucraft.Database.Client.Test.Program+<>c__DisplayClass0_0).email)
 
-            string email = "lucraft@lucraft.ddns.net";
-            string username = null;
-            bool verified = true;
+            //string email = "lucraft@lucraft.ddns.net";
+            //string username = null;
+            //bool verified = true;
             //string query = ToQueryString<Account>((account) => account.Email == email);
-            Console.WriteLine(LambdaTests.LambdaToString<Account>((account) => account.Email == email && account.Verified == verified && account.Username != username));
-            
+            //Console.WriteLine(LambdaTests.LambdaToString<Account>((account) => account.Email == email && account.Verified == verified && account.Username == username));
+
+            DataStorage.SetInstance("localhost");
+
+            CollectionReference collection = DataStorage.Instance.GetDatabase("website").GetCollection("accounts");
+
+            string email = "luca.lewin1203@gmail.com";
+            QuerySnapshot querySnapshot = collection.Query<Account>((account) => account.Email == email);
+
+            querySnapshot.Documents.ForEach((doc) =>
+            {
+                Console.WriteLine(doc.ID + " -> " + doc.ConvertTo<Account>().ToString());
+            });
+
+            DocumentReference reference = collection.GetDocument("st8dlZE25EqjVNy0rXacNg");
+            DocumentSnapshot snapshot = reference.Get();
+
+            Console.WriteLine(snapshot.ConvertTo<Account>().ToString());
+
         }
     }
 }
