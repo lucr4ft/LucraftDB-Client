a1790223f3e732befc48f6b8c9884d9b52cc6782
Luca Lewin <luca.lewin23@gmail.com>
2021-01-16 17:11:09 +0100

    create class RequestType + add constants

---
+++ LucraftDB-ClientAPI/RequestType.cs
@@ -0,0 +1,9 @@
+﻿namespace Lucraft.Database.Client
+{
+    internal class RequestType
+    {
+        public const string Get = "get";
+        public const string Set = "set";
+        public const string Delete = "delete";
+    }
+}
