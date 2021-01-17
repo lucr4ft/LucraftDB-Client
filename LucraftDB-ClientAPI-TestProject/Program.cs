b72a0abb51c8fe494be81f871927fd1ee07cc91c
Luca Lewin <luca.lewin23@gmail.com>
2021-01-17 14:38:10 +0100

    delete unused files

--- LucraftDB-ClientAPI-TestProject/Program.cs
+++ LucraftDB-ClientAPI-TestProject/Program.cs
@@ -7,32 +7,17 @@ namespace Lucraft.Database.Client.Test
     {
         static void Main()
         {
-
-            //(email == value(Lucraft.Database.Client.Test.Program+<>c__DisplayClass0_0).email)
-
-            //string email = "lucraft@lucraft.ddns.net";
-            //string username = null;
-            //bool verified = true;
-            //string query = ToQueryString<Account>((account) => account.Email == email);
-            //Console.WriteLine(LambdaTests.LambdaToString<Account>((account) => account.Email == email && account.Verified == verified && account.Username == username));
-
             DataStorage.SetInstance("localhost");
 
             CollectionReference collection = DataStorage.Instance.GetDatabase("website").GetCollection("accounts");
 
-            string email = "luca.lewin1203@gmail.com";
+            string email = "example@example.com";
             QuerySnapshot querySnapshot = collection.Query<Account>((account) => account.Email == email);
 
             querySnapshot.Documents.ForEach((doc) =>
             {
                 Console.WriteLine(doc.ID + " -> " + doc.ConvertTo<Account>().ToString());
             });
-
-            DocumentReference reference = collection.GetDocument("st8dlZE25EqjVNy0rXacNg");
-            DocumentSnapshot snapshot = reference.Get();
-
-            Console.WriteLine(snapshot.ConvertTo<Account>().ToString());
-
         }
     }
 }
