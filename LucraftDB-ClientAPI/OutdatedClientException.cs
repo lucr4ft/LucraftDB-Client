254b6b52a323171e796dca48aca3c4b06af01d69
lucalewin <luca.lewin23@gmail.com>
2021-01-31 20:59:16 +0100

    create exception OutdatedClientException

---
+++ LucraftDB-ClientAPI/OutdatedClientException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lucraft.Database.Client
+{
+    public class OutdatedClientException : Exception
+    {
+        public OutdatedClientException(string message) : base(message) { }
+    }
+}
